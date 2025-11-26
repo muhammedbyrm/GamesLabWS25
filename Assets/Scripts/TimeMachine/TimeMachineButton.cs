8c7c2564eb1806198b2eb75a1e0c5eefc48bff64
LouisSajuk <83764113+LouisSajuk@users.noreply.github.com>
2025-11-26 13:10:34 +0100

    added Sound and particle mechanics for TimeMachine
    It can fully be used as a prefab for later

--- Assets/Scripts/TimeMachine/TimeMachineButton.cs
+++ Assets/Scripts/TimeMachine/TimeMachineButton.cs
@@ -1,14 +1,26 @@
-using UnityEngine;
-using UnityEngine.Playables;
-using UnityEngine.Animations;
 using System.Collections;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.Audio;
+using UnityEngine.Playables;
 
 public class TimeMachineButton : MonoBehaviour
 {
     public Animator anim;
+    public Collider buttonCollider;
+    public AudioClip doorCloseSound;
+    public AudioClip doorOpenSound;
+    public AudioClip tmSound;
+    public ParticleSystem tmParticles;
 
     public void CloseTMDoor()
     {
+        buttonCollider.enabled = false;
+        tmParticles.Play();
+        PlayDoorCloseSound();
+
+        PlayTimeMachineSound();
+
         StartCoroutine(PlayRoutine());
     }
 
@@ -16,6 +28,24 @@ public class TimeMachineButton : MonoBehaviour
     {
         anim.SetTrigger("TM_Close");
 
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(3f);
+
+        buttonCollider.enabled = true;
+        PlayDoorOpenSound();
+    }
+
+    public void PlayDoorCloseSound()
+    {
+        SoundManager.Instance.PlaySoundClip(doorCloseSound, transform, 1f);
+    }
+
+    public void PlayDoorOpenSound()
+    {
+        SoundManager.Instance.PlaySoundClip(doorOpenSound, transform, 1f);
+    }
+
+    public void PlayTimeMachineSound()
+    {
+        SoundManager.Instance.PlaySoundClipPitched(tmSound, transform, 1f, 3f);
     }
 }
